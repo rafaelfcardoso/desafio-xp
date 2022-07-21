dae6933c7605f2c91cd3918a2d3da861de356503
Rafael <rafael.fcardoso@hotmail.com>
2022-07-21 11:09:22 -0300

    fix: removed local Database variables

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -8,8 +8,8 @@ import mysql from 'mysql2/promise'; // instalar mysql2 e dotenv
 dotenv.config();
 
 export default mysql.createPool({
-  host: process.env.MYSQL_HOST || "127.0.0.1",
-  user: process.env.MYSQL_USER || "Rafael",
-  password: process.env.MYSQL_PASSWORD || "@Rfc180603",
-  database: process.env.MYSQL_DATABASE || "desafio_xp"
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
