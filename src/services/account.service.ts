b0c69f6d6ef03b7756b4fa777b41a70ec2ef5884
Rafael <rafael.fcardoso@hotmail.com>
2022-07-24 22:53:18 -0300

    feat: field validation in createDeposit and createWithdraw

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,8 +1,31 @@
 import ITransaction from "../interfaces/transaction.interface";
 import accountModel from "../models/account.model";
+import HttpException from "../shared/http.exception";
+import { StatusCodes } from "http-status-codes";
+
+const isValidField = (order: ITransaction) => {
+  if (!order.codCliente || typeof order.codCliente !== "number") return false;
+  if (!order.valor || typeof order.valor !== "number") return false;
+
+  return true;
+};
+
+const isValid = (order: ITransaction) => {
+  if (order.valor <= 0) return false;
+
+  return true;
+};
 
 
 const createDeposit = async (deposit: ITransaction): Promise<ITransaction> => {
+  if (!isValidField(deposit)) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, "Dados inválidos!");
+  }
+
+  if (!isValid(deposit)) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, "Quantidade a ser depositada não poderá ser negativa ou igual a zero.");
+  }
+
   const { insertId } = await accountModel.createDeposit(deposit);
 
   const newDeposit = { ...deposit, id: insertId };
@@ -11,6 +34,14 @@ const createDeposit = async (deposit: ITransaction): Promise<ITransaction> => {
 }
 
 const createWithdraw = async (withdraw: ITransaction): Promise<ITransaction> => {
+  if (!isValidField(withdraw)) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, "Dados inválidos!");
+  }
+
+  if (!isValid(withdraw)) {
+    throw new HttpException(StatusCodes.BAD_REQUEST, "Quantidade a ser depositada não poderá ser negativa ou igual a zero.");
+  }
+
   const { insertId } = await accountModel.createWithdraw(withdraw);
 
   const newWithdraw = { ...withdraw, id: insertId };
